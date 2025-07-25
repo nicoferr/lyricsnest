89185d3de95edde4a9ad67fae1f9042db628ad4f
POR-MNS-09\Stagiaire <nicolas.ferrari@stagiairesmns.fr>
2025-07-25 13:39:56 +0200

    Ajout index

--- mylyrics/models.py
+++ mylyrics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    lyrics = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.created_at}"
+    
